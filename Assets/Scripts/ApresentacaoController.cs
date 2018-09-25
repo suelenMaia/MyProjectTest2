9255bc2a202bd42fbd353a590c07eca3cf0088e4
suelenMaia <suelen.sp95@gmail.com>
2018-09-25 12:51:24 -0300

    SuelenMaia
    post form

--- Assets/Scripts/ApresentacaoController.cs
+++ Assets/Scripts/ApresentacaoController.cs
@@ -1,21 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class ApresentacaoController : MonoBehaviour {
 
-    public InputField senha;
-    public Text txtsenha;
+    public InputField inputNome, inputSenha;
+    public Dropdown turma;
+    string txtnome, txtsenha, txtturma;
     // Use this for initialization
     private void Start()
     {
-        senha.ActivateInputField();
+        
     }
 
     public void GetSetText()
     {
-        txtsenha.text = senha.text;
-        Debug.Log(txtsenha);
+
+        txtnome = inputNome.text;
+        txtsenha = inputSenha.text;
+        txtturma = turma.captionText.text;
+        
+        Debug.Log("Turma: "+txtturma);
+        StartCoroutine(InserirAluno(txtnome, txtsenha, txtturma));
+    }
+
+    public IEnumerator InserirAluno(string nome, string senha, string turma)
+    {
+        WWWForm form = new WWWForm();
+        form.headers.Add("action", "insertNewStudent");
+        form.AddField("nome", nome);
+        form.AddField("senha", senha);
+        form.AddField("turma", turma);
+
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost/apiInsert.php", form);
+        yield return www.SendWebRequest();
+
+        if (www.error != null)
+        {
+            Debug.Log("Erro: " + www.error);
+        }
+        else
+        {
+            Debug.Log("All OK");
+            Debug.Log("Text: " + www.downloadHandler.text);
+        }
     }
 }
